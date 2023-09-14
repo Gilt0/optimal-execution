816623cda4f0e8958076b1f4617243d4ab65eac0
Gilt0 <gil-arnaud.coche.2006@polytechnique.org>
2023-09-15 06:08:27 +0900

    OrderBook added

--- cmd/optimalexecution.go/main.go
+++ cmd/optimalexecution.go/main.go
@@ -9,7 +9,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var binanceEndpoint = "wss://stream.binance.com:9443/ws/btcusdt@trade"
+func main() {
+	const ticker = "btcusdt"
+	// GetTrades(ticker)
+	GetOrderBooks(ticker)
+}
+
+const WS_ENDPOINT = "wss://stream.binance.com:9443/ws/"
 
 type BinanceMessage struct {
 	Method string   `json:"method"`
@@ -17,18 +23,33 @@ type BinanceMessage struct {
 	ID     int      `json:"id"`
 }
 
-func main() {
+type TradeResponse struct {
+	E  string `json:"e"`
+	Ev uint64 `json:"E"`
+	S  string `json:"s"`
+	T  uint64 `json:"t"`
+	P  string `json:"p"`
+	Q  string `json:"q"`
+	B  int64  `json:"b"`
+	A  int64  `json:"a"`
+	Tm uint64 `json:"T"`
+	M  bool   `json:"m"`
+	Mt bool   `json:"M"`
+}
+
+func GetTrades(ticker string) {
 	headers := http.Header{}
+	var binanceEndpoint = WS_ENDPOINT + ticker + "@trade"
 	conn, _, err := websocket.DefaultDialer.Dial(binanceEndpoint, headers)
 	if err != nil {
 		log.Fatal("Error connecting to Binance WebSocket: ", err)
 	}
 	defer conn.Close()
 
-	// Subscribe to btcusdt@trade
+	// Subscribe to <ticker>@trade
 	message := BinanceMessage{
 		Method: "SUBSCRIBE",
-		Params: []string{"btcusdt@trade"},
+		Params: []string{ticker + "@trade"},
 		ID:     1,
 	}
 	conn.WriteJSON(message)
@@ -40,12 +61,58 @@ func main() {
 			return
 		}
 
-		var response map[string]interface{}
+		var response TradeResponse
 		if err := json.Unmarshal(p, &response); err != nil {
 			log.Println("Error unmarshalling response: ", err)
 			continue
 		}
 
-		fmt.Printf("Trade: Price=%v at Time=%v\n", response["p"], response["T"])
+		fmt.Printf("Trade: Price=%v at Time=%v\n", response.P, response.Tm)
+	}
+}
+
+type OrderBookResponse struct {
+	LastUpdateID int64       `json:"lastUpdateId"`
+	Bids         [][2]string `json:"bids"`
+	Asks         [][2]string `json:"asks"`
+}
+
+func GetOrderBooks(ticker string) {
+	headers := http.Header{}
+	var binanceEndpoint = WS_ENDPOINT + ticker + "@depth20"
+	conn, _, err := websocket.DefaultDialer.Dial(binanceEndpoint, headers)
+	if err != nil {
+		log.Fatal("Error connecting to Binance WebSocket: ", err)
+	}
+	defer conn.Close()
+
+	// Subscribe to <ticker>@depth20
+	message := BinanceMessage{
+		Method: "SUBSCRIBE",
+		Params: []string{ticker + "@depth20"},
+		ID:     1,
+	}
+	conn.WriteJSON(message)
+
+	for {
+		_, p, err := conn.ReadMessage()
+		if err != nil {
+			log.Println("Error reading message: ", err)
+			return
+		}
+
+		var response OrderBookResponse
+		if err := json.Unmarshal(p, &response); err != nil {
+			log.Println("Error unmarshalling response: ", err)
+			continue
+		}
+
+		for _, bid := range response.Bids {
+			fmt.Printf("Bid: Price=%v, Quantity=%v\n", bid[0], bid[1])
+		}
+
+		for _, ask := range response.Asks {
+			fmt.Printf("Ask: Price=%v, Quantity=%v\n", ask[0], ask[1])
+		}
 	}
 }
